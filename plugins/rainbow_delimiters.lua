406442616e030b85cf64ee540d048537d38ea132
mrthere3 <871780968@qq.com>
2024-01-15 23:49:19 +0800

    update rainsbow plugin

--- plugins/rainbow_delimiters.lua
+++ plugins/rainbow_delimiters.lua
@@ -2,12 +2,13 @@ return {
   {
     "HiPhish/rainbow-delimiters.nvim",
     dependencies = "nvim-treesitter/nvim-treesitter",
-    event = "User AstroFile",
-    config = function(_, opts) require "rainbow-delimiters.setup"(opts) end,
+    event = "VeryLazy",
+    main = "rainbow-delimiters.setup",
   },
   {
     "catppuccin/nvim",
     optional = true,
+    ---@type CatppuccinOptions
     opts = { integrations = { rainbow_delimiters = true } },
   },
 }
