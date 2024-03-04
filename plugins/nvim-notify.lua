10548e8f80e5d4d769e480dda61da30746afd93e
mrthere3 <871780968@qq.com>
2024-03-04 21:55:13 +0800

    add snippets noice  transparent change notify time

---
+++ plugins/nvim-notify.lua
@@ -0,0 +1,11 @@
+return {
+  "rcarriga/nvim-notify",
+  opts = {
+    stages = "static",
+    -- render = "compact",
+    max_width = "40",
+    fps = 30,
+    level = 1,
+    timeout = 2000,
+  },
+}
