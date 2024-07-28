bf5912defde849e29e8ada97b85e9295ff1568f6
mrthere3 <871780968@qq.com>
2024-07-28 14:26:17 +0800

    change overseer  plugin

--- plugins/overseer.lua
+++ plugins/overseer.lua
@@ -3,18 +3,36 @@ return {
   dependencies = {
     {
       "stevearc/overseer.nvim",
-      opts = {
-        task_list = { -- this refers to the window that shows the result
-          direction = "bottom",
-          min_height = 15,
-          max_height = 25,
-          default_detail = 1,
-          bindings = {
-            ["q"] = function() vim.cmd "OverseerClose" end,
-          },
-        },
-      },
-      config = function(_, opts) require("overseer").setup(opts) end,
+      -- config = function()
+      --   local overseer = require "overseer"
+      --   overseer.setup {
+      --     dap = false,
+      --     templates = { "make", "cargo", "shell", "user.run_python", "user.run_script" },
+      --     task_list = {
+      --       direction = "bottom",
+      --       min_height = 15,
+      --       max_height = 25,
+      --       default_detail = 1,
+      --       bindings = {
+      --         ["q"] = function() vim.cmd "OverseerClose" end,
+      --         ["<C-u>"] = false,
+      --         ["<C-d>"] = false,
+      --         ["<C-h>"] = false,
+      --         ["<C-j>"] = false,
+      --         ["<C-k>"] = false,
+      --         ["<C-l>"] = false,
+      --       },
+      --     },
+      --   }
+      --   -- custom behavior of make templates
+      --   overseer.add_template_hook({
+      --     module = "^make$",
+      --   }, function(task_defn, util)
+      --     util.add_component(task_defn, { "on_output_quickfix", open_on_exit = "failure" })
+      --     util.add_component(task_defn, "on_complete_notify")
+      --     util.add_component(task_defn, { "display_duration", detail_level = 1 })
+      --   end)
+      -- end,
     },
   },
   cmd = { "CompilerOpen", "CompilerToggleResults" },
--- plugins/overseer_nvim.lua
+++ plugins/overseer_nvim.lua
@@ -17,6 +17,28 @@ return {
       "OverseerClearCache",
     },
     opts = {},
+    config = function()
+      local overseer = require "overseer"
+      overseer.setup {
+        dap = false,
+        templates = { "make", "cargo", "shell", "user.run_python", "user.run_script" },
+        task_list = {
+          direction = "bottom",
+          min_height = 15,
+          max_height = 25,
+          default_detail = 1,
+          bindings = {
+            ["q"] = function() vim.cmd "OverseerClose" end,
+            ["<C-u>"] = false,
+            ["<C-d>"] = false,
+            ["<C-h>"] = false,
+            ["<C-j>"] = false,
+            ["<C-k>"] = false,
+            ["<C-l>"] = false,
+          },
+        },
+      }
+    end,
   },
   {
     "catppuccin/nvim",
